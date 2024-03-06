ad9e98747cfdcafde6af57c65e4ebb23027000c9
HajarahM <108297190+HajarahM@users.noreply.github.com>
2024-03-06 05:27:50 +0000

    testing gradient app first try

---
+++ app.py
@@ -0,0 +1,43 @@
+
+from gradientai import Gradient
+import os
+import requests
+import streamlit as st
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+COLLECTIONID = st.secrets.collectionid
+WORKSPACEID = st.secrets.workspaceid
+AUTHORIZATION = st.secrets.autorization
+
+def main() -> None:
+	gradient = Gradient()
+	base_model = gradient.get_base_model(base_model_slug="mixtral-8x7b-instruct")
+	st.title ('Uganda Law Chatbot')
+	question = st.text_input ('Ask a question:')
+
+	url = "https://api.gradient.ai/api/blocks/answer"
+
+	if question:
+	
+		payload = {
+    			"source": {
+        		"type": "rag",
+        		"collectionId": COLLECTIONID
+    				},
+    			"question": question
+			}
+		headers = {
+    			"accept": "application/json",
+    			"x-gradient-workspace-id": WORKSPACEID,
+    			"content-type": "application/json",
+    			"authorization": AUTHORIZATION
+			}
+		
+		response = requests.post(url, json=payload, headers=headers)
+
+		st.write(response.text)
+
+if __name__ == "__main__":
+	main()
